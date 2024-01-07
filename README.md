43983d1867909fb965c4dc812c722b4491cba0b9
ziginfo <34913387+ziginfo@users.noreply.github.com>
2024-01-07 16:33:18 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 ## Mixingstation-Chataigne-module
 
-OSC-API must be enabled in the Mixingstation Globals-Setup !   
-Default OSC Out-Port is 3000 but can be changed in the Mixingstation Globals-Setup
+OSC-API must be enabled in the Mixingstation Globals-Setup !    
+Default OSC Out-Port is 3000 but can be changed in the Mixingstation Globals-Setup    
+
+###  How to use this...?  
+Launch Mixingstation and insert any given Mixer ! You can than go "Online" to the mixer (if you want) or stay "offline". It doesn't matter for the workaround with Chataigne, but be aware that, when Mixingstation is "online" to a given mixer, than every action in Chataigne will act directly also in the Mixer !! While, in offline-mode the interaction concerns only the graphical interface (GUI) of Mixingstation !! This can be a way for example to remotely interact from an iPad (or any tablette or smartphone) to Chataigne and so to any hardware or software that is connected to Chataigne...   
+
+First of all you should know that when using directly the Mixingstation-Module instead of the concerned Mixer-Chataigne-Modules, than Mixingstation count all available channels and numbers them from 0 to "last" without any distinction between Mix-Channel, Aux, Bus, Matrix, Main, DCA etc etc etc ! They are all just Channels => just as an example with an X18 mixer inserted to Mixingstation the four FX-Return-Channels are Channe 20 to 23 and the four DCA are 33 to 36 and the Main-LR Channel is number 32 etc etc... 
+
+after having inserted a given mixer in Mixingstation (and before using the Chataigne-Module) you might first see the number of the available channels and the the available actions and functions... this can be done (and seen) on the Mixingstation's API-Web-Page and the address : 
+http://localhost:8080/# and http://localhost:8080/#/data-explorer
--- mixingstation.js
+++ mixingstation.js
@@ -48,11 +48,15 @@ function moduleValueChanged(value) {
 function update(deltaTime) {
 		var now = util.getTime();
 		if(now > TSSendAlive) {
-		TSSendAlive = now + 5;
+		TSSendAlive = now + 2;
 		keepAlive(); }
 }
 function keepAlive() {
-		local.send("/hi/n"); 
+		if (val == 0){
+		local.send("/hi/n");
+		val= val+1; }
+		else {local.send("/hi/v");
+		val=0 ;}
 }
 
 
@@ -75,3 +79,126 @@ function oscEvent(address, args) {
 //=========================================================
 //					 REGULAR FUCNTIONS
 //=========================================================
+
+//  ======================== CHANNEL ========================
+
+function mix_fader(chanNo, val) {
+	var chanNo = chanNo-1 ;	
+	local.send("/con/n/ch."+chanNo+".mix.lvl", val);
+}
+
+function mix_on(chanNo, val) {
+	var chanNo = chanNo-1 ;	
+	val=1-val ;
+	local.send("/con/n/ch."+chanNo+".mix.on", val);
+}
+
+function mix_st(chanNo, val) {
+	var chanNo = chanNo-1 ;		
+	local.send("/con/n/ch."+chanNo+".mix.stOn", val);	
+}
+
+function mix_pan(chanNo, val) {
+	var chanNo = chanNo-1 ;	
+	local.send("/con/n/ch."+chanNo+".mix.pan", val);
+}
+
+function ch_solo(chanNo, val) {
+	var chanNo = chanNo-1 ;	
+	local.send("/con/n/ch."+chanNo+".solo", val);
+}
+
+function mix_send_level(chanNo, mix, val) {	
+	var chanNo = chanNo-1 ;	
+	var mix = mix-1 ;	
+	local.send("/con/n/ch."+chanNo+".mix.sends."+mix+".lvl", val); 
+}
+
+// ====================== EQ =====================
+
+function full_ch_eq (chanNo, val, val1, band, val2, val3, val4) {
+	chanNo = chanNo-1 ;
+	band = band -1 ;	
+	local.send("/con/n/ch."+chanNo+".peq.on", val1);
+	local.send("/con/v/ch."+chanNo+".peq.bands."+band+".gain", val2);
+	local.send("/con/v/ch."+chanNo+".peq.bands."+band+".freq", val3);
+	local.send("/con/v/ch."+chanNo+".peq.bands."+band+".q", val4);
+}
+
+function ch_eq_reset(chanNo) {
+	chanNo = chanNo-1 ;
+	local.send("/con/v/ch."+chanNo+".peq.bands.0.gain", 0);
+	local.send("/con/v/ch."+chanNo+".peq.bands.1.gain", 0);
+	local.send("/con/v/ch."+chanNo+".peq.bands.2.gain", 0);
+	local.send("/con/v/ch."+chanNo+".peq.bands.3.gain", 0);
+	local.send("/con/v/ch."+chanNo+".peq.bands.4.gain", 0);
+	local.send("/con/v/ch."+chanNo+".peq.bands.5.gain", 0);
+	local.send("/con/v/ch."+chanNo+".peq.bands.0.freq", 50);
+	local.send("/con/v/ch."+chanNo+".peq.bands.1.freq", 80);
+	local.send("/con/v/ch."+chanNo+".peq.bands.2.freq", 500);
+	local.send("/con/v/ch."+chanNo+".peq.bands.3.freq", 1000);
+	local.send("/con/v/ch."+chanNo+".peq.bands.4.freq", 3000);
+	local.send("/con/v/ch."+chanNo+".peq.bands.5.freq", 10000);
+}
+
+function eq_on(chanNo, val) {
+	chanNo = chanNo-1 ;
+	local.send("/con/n/ch."+chanNo+".peq.on", val);
+}
+
+
+function eq_f(chanNo, band, val) { 
+	chanNo = chanNo-1 ;
+	band = band -1 ;
+	local.send("/con/v/ch."+chanNo+".peq.bands."+band+".freq", val);
+}
+
+function eq_g(chanNo, band, val) {
+	chanNo = chanNo-1 ;
+	band = band -1 ; 
+	local.send("/con/n/ch."+chanNo+".peq.bands."+band+".gain", val);
+}
+
+function eq_q(chanNo, band, val) {
+	chanNo = chanNo-1 ;
+	band = band -1 ; 
+	local.send("/con/v/ch."+chanNo+".peq.bands."+band+".q", val);
+}
+
+// ================ LO CUT ------>>>>>
+
+function hipass (chanNo, val1, val2) {
+	chanNo = chanNo-1 ;
+	local.send("/con/n/ch."+chanNo+".preamp.filter.0.on", val1);
+	local.send("/con/v/ch."+chanNo+".preamp.filter.0.freq", val2);
+}
+
+function preamp_hpon(chanNo, val) {
+	chanNo = chanNo-1 ;
+	local.send("/con/n/ch."+chanNo+".preamp.filter.0.on", val);
+}
+
+function preamp_hpf(chanNo, val) {
+	chanNo = chanNo-1 ;  
+
+	local.send("/con/v/ch."+chanNo+".preamp.filter.0.freq", val);
+}
+
+// ================ HI CUT ------>>>>>
+
+function lopass (chanNo, val1, val2) {
+	chanNo = chanNo-1 ;
+	local.send("/con/n/ch."+chanNo+".preamp.filter.1.on", val1);
+	local.send("/con/v/ch."+chanNo+".preamp.filter.1.freq", val2);
+}
+
+function preamp_lpon(chanNo, val) {
+	chanNo = chanNo-1 ;
+	local.send("/con/n/ch."+chanNo+".preamp.filter.1.on", val);
+}
+
+function preamp_lpf(chanNo, val) {
+	chanNo = chanNo-1 ;  
+
+	local.send("/con/v/ch."+chanNo+".preamp.filter.1.freq", val);
+}
