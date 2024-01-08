6f1b27d3d70c30dc038df520e50eb0a6d24008c2
ziginfo <34913387+ziginfo@users.noreply.github.com>
2024-01-08 15:10:28 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,20 +1,22 @@
 ## Mixingstation-Chataigne-module
 
 OSC-API must be enabled in the Mixingstation Globals-Setup !    
-Default OSC Out-Port is 3000 but can be changed in the Mixingstation Globals-Setup    
+Default OSC Out-Port is 3000 but can be changed in the Mixingstation Globals-Setup 
+
+important : after inserting this Mixingstation-Module for the first time, the Containers (Faders, Labels, Buttons etc) will not show up ! You must save the session and reload it !   
+keyboard-shortcuts for this are :  cmd-S and than : cmd-shift-O   
+the same thing (save and reload) must be done when you eventually want to change the number of the channels !
 
 ###  How to use this...?  
-Launch Mixingstation and insert any given Mixer ! You can than go "Online" to the mixer (if you want) or stay "offline". It doesn't matter for the workaround with Chataigne, but be aware that, when Mixingstation is "online" to a given mixer, than every action in Chataigne will act directly also in the Mixer !! While, in offline-mode the interaction concerns only the graphical interface (GUI) of Mixingstation !!    
+Launch Mixingstation and insert any given Mixer ! You can than (if you want) go "online" to the mixer  or stay "offline". It doesn't matter for the workaround with Chataigne, but be aware that, when Mixingstation is "online" to a given mixer, than every action in Chataigne will obviously also trigger actions directly  in the Hardware-Mixer !! While, in offline-mode the interaction concerns only the graphical interface (GUI) of Mixingstation !!    
 This can be a way for example to remotely interact from an iPad (or any tablette or smartphone) to Chataigne and so also to any hardware or software that is connected to Chataigne...   
 
-First of all you should know that when using directly the Mixingstation-Module instead of the concerned Mixer-Chataigne-Modules, than Mixingstation count all available channels and numbers them from 0 to "last" without any distinction between Mix-Channel, Aux, Bus, Matrix, Main, DCA etc etc etc ! They are all just Channels => just as an example with an X18 mixer inserted to Mixingstation the four FX-Return-Channels are Channels 20 to 23 and the four DCAs are 33 to 36 and the Main-LR Channel is number 32 etc etc...    
+First of all you should know that when using directly the Mixingstation-Module instead of the concerned Mixer-Chataigne-Modules, than Mixingstation count all available (in and out) channels and numbers them from 0 to "last" without any distinction between Mix-Channel, Aux, Bus, FX-Returns, Matrix, Main, DCA etc etc etc ! They are all just "Channels" => just as an example with an X18 mixer inserted to Mixingstation the four FX-Return-Channels are Channe 20 to 23 and the four DCA are 33 to 36 and the Main-LR Channel is number 32 etc etc...   
 Depending of what mixer is inserted, the number of available Channel can vary between 20 and 300 !!. So, if you insert à M32-Mixer, you now will have 79 available channels !! and when inserting a Midas Heritage HD96 Mixer you will have some 291 virtual channels available !!
 
-After having inserted a given mixer in Mixingstation (and before using the Chataigne-Module) you might first see the number of the available channels and the available actions and functions... this can be done (and seen) on the Mixingstation's API-Web-Page and the address :    
-http://localhost:8080/#       
-http://localhost:8080/#/data-explorer    
+After having inserted a given mixer in Mixingstation (and before using the Chataigne-Module) you may first wanna see the number of the available channels and the available actions and functions... this can be done (and seen) on the Mixingstation's API-Web-Page and the address :   
+http://localhost:8080/#   
+http://localhost:8080/#/data-explorer   
 http://localhost:8080/#/rest-api    
 
 Beside the  predefined actions of the actions-menu in the Chataigne-Module, you can also use any available Custom-Message... please go to the #/data-explorer at localhost to see what is available for any given mixer.
-
-This module is still "WiP" and will be extended in the next few weeks...
--- mixingstation.js
+++ mixingstation.js
@@ -1,28 +1,48 @@
 // ========================== VARS ===========================
 var myParameters = {};
-
+var count ;
 
 //====================================================================
 //						INITIAL FUNCTIONS 
 //====================================================================
 
 function init() {
-  script.log("Custom module init");
 
 // =====================================================================
 // 						CREATE CONTAINERS
 // =====================================================================
 			
-// Faders Container>>>>>>>>>>>>>>>>>>>>>		
-	faders = local.values.addContainer("Faders");
+// Faders Container>>>>>>>>>>>>>>>>>>>>>
+
+		chanNumb = local.values.addIntParameter("Number of Channels", "Set Number of Channels" , 16, 16);
+		
+		faders = local.values.addContainer("Faders");
 		faders.setCollapsed(true);
-		faders.addTrigger("Reset", "Reset All Values" , false);
-		faders.addTrigger("Sync Faders", "Get Fader Values from the Console" , false);	
-		for (var n = 1; n < 37; n++) {
+		count = local.values.numberOfChannels.get() ;  	
+		faders.addTrigger("Reset Values", "Reset All Values" , false);
+		faders.addTrigger("Sync Faders", "Get Fader Values from Mixingstation" , false);	
+		for (var n = 1; n <= count; n++) {
 			var fade = faders.addFloatParameter("Fader "+n, "", 0, 0, 1); 
 //			fade.setAttribute("readonly" ,true);
-			}   
-  
+			}
+			
+//Labels Container >>>>>>>>>>>>>>>>>>>>>>		
+		labels=local.values.addContainer("Labels");
+		labels.setCollapsed(true);
+		labels.addTrigger("Reset Labels", "Reset All Labels" , false);
+		labels.addTrigger("Sync Labels", "Get Labels from Mixingstation" , false);	
+		for (var n = 1; n <= count; n++) {
+			labels.addStringParameter("Label "+n, "", ""); }
+		 
+ //Buttons Container >>>>>>>>>>>>>>>>>>>>>>		
+		buttons=local.values.addContainer("Buttons");
+		buttons.setCollapsed(true);
+		buttons.addTrigger("Reset Buttons", "Reset All Buttons" , false);
+		buttons.addTrigger("Sync Buttons", "Get Buttons from Mixingstation" , false);	
+		for (var n = 1; n <= count; n++) {
+			buttons.addBoolParameter("On "+n, "", false); }
+		for (var n = 1; n <= count; n++) {
+			buttons.addBoolParameter("Solo "+n, "", false);  }
 }
 
 //============================================================
@@ -32,17 +52,36 @@ function init() {
 function moduleValueChanged(value) {
 
   	if (value.name == "syncFaders"){ 
-  	for (var n = 0; n < 36; n++) {
+  	for (var n = 0; n < count; n++) {
    	var no = n + 1 ;
-   	local.send("/con/n/ch."+n+".mix.lvl");} 
-  	}
-  
-	if (value.name == "reset"){ 
-   	for (var n = 0; n < 36; n++) {
+  	local.send("/con/n/ch."+n+".mix.lvl"); 	} }
+   	
+ 	if (value.name == "syncLabels"){ 
+  	for (var n = 0; n < count; n++) {
    	var no = n + 1 ;
-	local.values.faders.getChild('fader'+no).set(0.0);} }
-   
-   
+   	local.send("/con/v/ch."+n+".cfg.name"); } }
+   	
+   	if (value.name == "syncButtons"){ 
+  	for (var n = 0; n < count; n++) {
+   	var no = n + 1 ;
+   	local.send("/con/n/ch."+n+".mix.on");
+   	local.send("/con/n/ch."+n+".solo"); } }
+   	
+	if (value.name == "resetValues"){ 
+   	for (var n = 0; n < count; n++) {
+   	var no = n + 1 ;
+	local.values.faders.getChild('fader'+no).set(0.0); } }
+	
+   	if (value.name == "resetLabels"){ 
+   	for (var n = 0; n < count; n++) {
+   	var no = n + 1 ;
+	local.values.labels.getChild('label'+no).set("");  } }
+	
+   	if (value.name == "resetButtons"){ 
+   	for (var n = 0; n < count; n++) {
+   	var no = n + 1 ;
+	local.values.buttons.getChild('on'+no).set(0);
+	local.values.buttons.getChild('solo'+no).set(0);  } }
 }
 
 //============================================================
@@ -69,16 +108,36 @@ function keepAlive() {
 
 function oscEvent(address, args) {
 	
-	for (var n = 0; n < 36; n++) {
+	for (var n = 0; n <= count; n++) {
 	var no = n+1 ;
 	var addr = "/con/n/ch."+n+".mix.lvl" ;
 	var child = "fader"+no ;
 	if (address == addr) {
 	local.values.faders.getChild('fader'+no).set(args[0]); } }
 	
+	for (var n = 0; n <= count; n++) {
+	var no = n+1 ;
+	var addr = "/con/v/ch."+n+".cfg.name" ;
+	var child = "label"+no ;
+	if (address == addr) {
+	local.values.labels.getChild('label'+no).set(args[0]); } }
 	
+	for (var n = 0; n <= count; n++) {
+	var no = n+1 ;
+	var addr = "/con/n/ch."+n+".mix.on" ;
+	var child = "on"+no ;
+	if (address == addr) {
+	local.values.buttons.getChild('on'+no).set(args[0]);} }
+	
+	for (var n = 0; n <= count; n++) {
+	var no = n+1 ;
+	var addr = "/con/n/ch."+n+".solo" ;
+	var child = "solo"+no ;
+	if (address == addr) {
+	local.values.buttons.getChild('solo'+no).set(args[0]);} }
 }
 
+/*
 //=========================================================
 //					 REGULAR FUCNTIONS
 //=========================================================
@@ -91,30 +150,36 @@ function mix_fader(chanNo, val) {
 }
 
 function mix_on(chanNo, val) {
-	var chanNo = chanNo-1 ;	
+	chanNo = chanNo-1 ;	
 	val=1-val ;
 	local.send("/con/n/ch."+chanNo+".mix.on", val);
 }
 
 function mix_st(chanNo, val) {
-	var chanNo = chanNo-1 ;		
+	chanNo = chanNo-1 ;		
 	local.send("/con/n/ch."+chanNo+".mix.stOn", val);	
 }
 
 function mix_pan(chanNo, val) {
-	var chanNo = chanNo-1 ;	
+	chanNo = chanNo-1 ;	
 	local.send("/con/n/ch."+chanNo+".mix.pan", val);
 }
 
 function ch_solo(chanNo, val) {
-	var chanNo = chanNo-1 ;	
+	chanNo = chanNo-1 ;	
 	local.send("/con/n/ch."+chanNo+".solo", val);
 }
 
 function mix_send_level(chanNo, mix, val) {	
-	var chanNo = chanNo-1 ;	
-	var mix = mix-1 ;	
+	chanNo = chanNo-1 ;	
+	mix = mix-1 ;	
 	local.send("/con/n/ch."+chanNo+".mix.sends."+mix+".lvl", val); 
+}
+
+
+function ch_label(chanNo, val) {
+	chanNo = chanNo-1 ;
+	local.send("/con/v/ch."+chanNo+".cfg.name", val); 
 }
 
 // ====================== EQ =====================
@@ -205,3 +270,5 @@ function preamp_lpf(chanNo, val) {
 
 	local.send("/con/v/ch."+chanNo+".preamp.filter.1.freq", val);
 }
+
+*/
